d850efeca700f3f251122422cc0e5b6e1685c52f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-13 20:23:43 +0530

    Add project files.

---
+++ sad/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.AccessibleName = "btnStart"
+        Button1.Location = New Point(272, 142)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(205, 102)
+        Button1.TabIndex = 0
+        Button1.Text = "Click Me"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.AccessibleName = "txtNumber1"
+        TextBox1.Location = New Point(284, 42)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.AccessibleName = "txtNumber2"
+        TextBox2.Location = New Point(284, 86)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AccessibleName = "lblNumber1"
+        Label1.AutoSize = True
+        Label1.Location = New Point(209, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Number 1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AccessibleName = "lblNumber2"
+        Label2.AutoSize = True
+        Label2.Location = New Point(203, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(75, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Number 2"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ sad/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim iNumber1 As Integer
+        Dim iNumber2 As Integer
+
+        Dim iResult As Integer
+
+        iNumber1 = TextBox1.Text
+        iNumber2 = TextBox2.Text
+
+        iResult = iNumber1 + iNumber2
+        MsgBox(iResult)
+
+
+
+
+
+
+    End Sub
+
+
+
+
+End Class
