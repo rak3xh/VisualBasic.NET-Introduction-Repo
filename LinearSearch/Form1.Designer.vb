6b23e213b05169110eee7ae7e9c910cd55f03a69
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-18 19:16:58 +0530

    Second Commit

---
+++ LinearSearch/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnbLinearSeach = New Button()
+        SuspendLayout()
+        ' 
+        ' btnbLinearSeach
+        ' 
+        btnbLinearSeach.Location = New Point(302, 125)
+        btnbLinearSeach.Name = "btnbLinearSeach"
+        btnbLinearSeach.Size = New Size(204, 122)
+        btnbLinearSeach.TabIndex = 0
+        btnbLinearSeach.Text = "Linear Search"
+        btnbLinearSeach.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnbLinearSeach)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnbLinearSeach As Button
+
+End Class
---
+++ LinearSearch/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub btnbLinearSeach_Click(sender As Object, e As EventArgs) Handles btnbLinearSeach.Click
+        Dim numbers(20) As Integer
+        Dim n As Integer
+        n = InputBox("Enter the size of the Array")
+        Dim i As Integer
+        For i = 0 To n - 1
+            numbers(i) = InputBox("enter the element" & (i + 1) & "of array")
+        Next
+
+
+        ' Value to search for
+        Dim searchValue As Integer
+        searchValue = InputBox("Enter the number to be searched")
+
+
+        ' Perform linear search
+        Dim index As Integer = LinearSearch(numbers, searchValue)
+
+        ' Output the result
+        If index <> -1 Then
+            ' Console.WriteLine("Value " & searchValue & " found at index " & index)
+            MsgBox("Found at " & index + 1 & " Position")
+        Else
+            'Console.WriteLine("Value " & searchValue & " not found in the array")
+            MsgBox("Not Found")
+        End If
+    End Sub
+    Function LinearSearch(ByVal arr() As Integer, ByVal value As Integer) As Integer
+        ' Iterate through the array
+        For i As Integer = 0 To arr.Length - 1
+            ' Check if the current element is the one we're looking for
+            If arr(i) = value Then
+                ' Return the index if found
+                Return i
+            End If
+        Next
+
+        ' Return -1 if the value is not found
+        Return -1
+    End Function
+
+
+End Class
