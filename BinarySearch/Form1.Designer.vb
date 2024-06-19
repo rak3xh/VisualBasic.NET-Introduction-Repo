ff5cc20f9d0758e8b7272dce1f920b123d3db805
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-19 19:38:32 +0530

    Third Commit

---
+++ BinarySearch/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnBinarySearch = New Button()
+        SuspendLayout()
+        ' 
+        ' btnBinarySearch
+        ' 
+        btnBinarySearch.Location = New Point(279, 151)
+        btnBinarySearch.Name = "btnBinarySearch"
+        btnBinarySearch.Size = New Size(221, 108)
+        btnBinarySearch.TabIndex = 0
+        btnBinarySearch.Text = "Binary Search"
+        btnBinarySearch.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnBinarySearch)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnBinarySearch As Button
+
+End Class
---
+++ BinarySearch/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub btnBinarySearch_Click(sender As Object, e As EventArgs) Handles btnBinarySearch.Click
+        Dim numbers(20) As Integer
+        Dim n As Integer
+        n = InputBox("Enter the size of the Array")
+        Dim i As Integer
+        For i = 0 To n - 1
+            numbers(i) = InputBox("enter the element " & (i + 1) & " of array")
+        Next
+
+
+        ' Value to search for
+        Dim searchValue As Integer
+        searchValue = InputBox("Enter the number to be searched")
+
+
+        ' Perform linear search
+        Dim index As Integer = BinarySearch(numbers, searchValue, n)
+
+        ' Output the result
+        If index <> -1 Then
+            ' Console.WriteLine("Value " & searchValue & " found at index " & index)
+            MsgBox("Found at " & index + 1 & " Position")
+        Else
+            'Console.WriteLine("Value " & searchValue & " not found in the array")
+            MsgBox("Not Found")
+        End If
+    End Sub
+    Function BinarySearch(ByVal arr() As Integer, ByVal value As Integer, ByVal n As Integer) As Integer
+        Dim first As Integer = 0
+        Dim last As Integer = n - 1
+        Dim middle As Integer = (first + last) / 2
+
+        While (first <= last)
+            If arr(middle) = value Then
+                Return middle
+            ElseIf arr(middle) < value Then
+                first = middle + 1
+            Else
+                last = middle - 1
+            End If
+            middle = (first + last) / 2
+        End While
+        Return -1
+    End Function
+
+
+End Class
